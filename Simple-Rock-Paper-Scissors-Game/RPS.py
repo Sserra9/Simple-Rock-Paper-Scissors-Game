c2999adee85c5b0b1d9dcca6b63b9e469864aae0
Sean Serra <69557379+Sserra9@users.noreply.github.com>
2020-08-14 16:15:05 -0400

    Add files via upload

---
+++ Simple-Rock-Paper-Scissors-Game/RPS.py
@@ -0,0 +1,33 @@
+#This is my first project. I followed a guide online....
+from random import randint
+play_options=['Rock','Paper','Scissors']
+
+computer=play_options[randint(0,2)]
+
+player=False
+
+while player == False:
+
+    player=input('Rock','Paper','Scissors?')
+    if player == computer:
+        print("Tie!")
+    elif player == 'Rock':
+        if computer == 'Rock':
+            print('You lose!', computer, 'covers', player)
+        else:
+            print('You win!', player, 'smashes', computer)
+    elif player == 'Paper':
+        if computer == 'Scissors':
+            print('You lose!', computer, 'cuts', player)
+        else:
+            print('You win!', player, 'covers', computer)
+    elif player == 'Scissors'
+        if computer == 'Rock':
+            print('You lose!', computer,'smashes',player)
+        else:
+            print('You win!', player, 'cuts', computer)
+    else:
+        print('That\'s not a valid play. Check your spelling.')
+        
+player = False
+computer=play_options[randint(0,2)]
